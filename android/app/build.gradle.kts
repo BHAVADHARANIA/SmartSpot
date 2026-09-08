7c11de74117d5f56102ae98f672a892752840f73
BHAVADHARANIA <175288333+BHAVADHARANIA@users.noreply.github.com>
2026-09-08 11:35:47 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,14 +25,14 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
+       buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // signingConfig = signingConfigs.getByName("debug")
         }
     }
-}
+
 
 kotlin {
     compilerOptions {
